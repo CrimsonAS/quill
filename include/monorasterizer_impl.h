04611142d79e4a4f32f7c89eb092acd3c0b8be99
Gunnar Sletta <gunnar@crimson.no>
2017-08-26 13:09:39 +0200

    basic working code, though no joins or caps, yet

---
+++ include/monorasterizer_impl.h
@@ -0,0 +1,88 @@
+/*
+    Copyright (c) 2017, Gunnar Sletta <gunnar@crimson.no>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+template <typename ScanlineConsumer>
+void MonoRasterizer<ScanlineConsumer>::operator()(Triangle t)
+{
+    t.sort();
+
+    float y0Floored = std::floor(t.y0);
+    float y;
+
+    if (t.y0 - y0Floored < 0.5) {
+        y = y0Floored + 0.5;
+    } else {
+        y = y0Floored + 1.5;
+    }
+
+    if (t.y0 != t.y1) {
+        float dy01 = (t.y1 - t.y0);
+        float dy02 = (t.y2 - t.y0);
+        float dxl = (t.x1 - t.x0) / dy01;
+        float dxr = (t.x2 - t.x0) / dy02;
+        float yoffset = y - t.y0;
+        float left = t.x0 + dxl * yoffset;
+        float right = t.x0 + dxr * yoffset;
+        if (left > right) {
+            std::swap(left, right);
+            std::swap(dxl, dxr);
+        }
+        iterate(y, t.y1, left, right, dxl, dxr);
+    }
+
+    if (t.y1 != t.y2) {
+        float dy02 = (t.y2 - t.y0);
+        float dy12 = (t.y2 - t.y1);
+        float dxl = (t.x2 - t.x1) / dy12;
+        float dxr = (t.x2 - t.x0) / dy02;
+        float yoffset = y - t.y1;
+        float left = t.x2 - (t.y2 - y) * dxl;
+        float right = t.x2 - (t.y2 - y) * dxr;
+        if (left > right) {
+            std::swap(left, right);
+            std::swap(dxl, dxr);
+        }
+        iterate(y, t.y2, left, right, dxl, dxr);
+     }
+}
+
+template <typename ScanlineConsumer>
+void MonoRasterizer<ScanlineConsumer>::iterate(float &y, float ymax, float left, float right, float leftIncr, float rightIncr)
+{
+     while (y < ymax) {
+         int l = (int) (left + 0.5);
+         int r = (int) (right + 0.5);
+         int len = r - l;
+         assert(len >= 0);
+         if (len > 0) {
+             scan(int(y), l, r-l, 255);
+         }
+         y += 1.0f;
+         left += leftIncr;
+         right += rightIncr;
+     }
+}
---
+++ include/triangle_impl.h
@@ -0,0 +1,79 @@
+/*
+    Copyright (c) 2017, Gunnar Sletta <gunnar@crimson.no>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+
+
+inline Triangle::Triangle()
+{
+}
+
+
+
+inline Triangle::Triangle(float x0_, float y0_, float x1_, float y1_, float x2_, float y2_, bool edge01_, bool edge02_, bool edge12_)
+    : x0(x0_)
+    , y0(y0_)
+    , x1(x1_)
+    , y1(y1_)
+    , x2(x2_)
+    , y2(y2_)
+    , edge01(edge01_)
+    , edge02(edge02_)
+    , edge12(edge12_)
+{
+}
+
+
+
+inline void Triangle::sort()
+{
+    if (y1 < y0) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+        std::swap(edge02, edge12);
+    }
+    if (y2 < y0) {
+        std::swap(x0, x2);
+        std::swap(y0, y2);
+        std::swap(edge01, edge12);
+    }
+    if (y2 < y1) {
+        std::swap(x1, x2);
+        std::swap(y1, y2);
+        std::swap(edge01, edge02);
+    }
+}
+
+
+
+inline std::ostream &operator<<(std::ostream &o, Triangle t)
+{
+    o << "triangle(" << t.x0 << "," << t.y0
+      << " - " << t.x1 << "," << t.y1
+      << " - " << t.x2 << "," << t.y2 << ")";
+    return o;
+}
+
