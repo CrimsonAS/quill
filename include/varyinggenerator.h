7b625a4d2529d9d37f1eceedec06a4e1aa2950ac
Gunnar Sletta <gunnar@crimson.no>
2017-09-29 11:31:11 +0200

    Fix licensing

--- include/varyinggenerator.h
+++ include/varyinggenerator.h
@@ -1,5 +1,6 @@
 /*
     Copyright (c) 2017, Gunnar Sletta <gunnar@crimson.no>
+    Copyright (c) 2017, reMarkable AS <technology@remarkable.no>
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
