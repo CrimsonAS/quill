CrimsonAS/quill

commit eccc66e9ad05034070e180aa93024e26c9273460

expose functions to flush start/end caps
Previously this was only possible by accessing the internals and
manually calling cap(), which is pretty ugly and not nice. Exposing
these functions which can be called whenever with minimal prior
knowledge cleans up other code a tiny bit.

include/quill_stroker_impl.h
@@ -93,8 +93,8 @@ void Stroker<Rasterizer, VaryingGenerator>::moveTo(float x, float y)
 {
     // std::cout << "moveTo(" << x << ", " << y << ")" << std::endl;
     if (m_lastSegment.type == LineToSegment) {
-        cap(m_firstLeft, m_firstRight, m_firstSegment, false);
-        cap(m_lastLeft, m_lastRight, m_lastSegment, true);
+        flushStartCap();
+        flushEndCap();
     }
 
     store(x, y, MoveToSegment, varying.left(length, width / 2), varying.right(length, width / 2));
@@ -304,6 +304,29 @@ void Stroker<Rasterizer, VaryingGenerator>::cap(Line left, Line right, Segment s
 }
 
 
+
+template <typename Rasterizer, typename VaryingGenerator>
+void Stroker<Rasterizer, VaryingGenerator>::flushStartCap()
+{
+    if (!m_startCapRendered && length > 0.0f) {
+        cap(m_firstLeft, m_firstRight, m_firstSegment, false);
+        m_startCapRendered = true;
+    }
+}
+
+
+
+template <typename Rasterizer, typename VaryingGenerator>
+void Stroker<Rasterizer, VaryingGenerator>::flushEndCap()
+{
+    if (!m_endCapRendered && length > 0.0f) {
+        cap(m_lastLeft, m_lastRight, m_lastSegment, true);
+        m_endCapRendered = true;
+    }
+}
+
+
+
 template <typename Rasterizer, typename VaryingGenerator>
 void Stroker<Rasterizer, VaryingGenerator>::close()
 {
@@ -331,8 +354,8 @@ void Stroker<Rasterizer, VaryingGenerator>::finish()
         if (m_lastSegment.x == m_firstSegment.x && m_lastSegment.y == m_firstSegment.y) {
             close();
         } else {
-            cap(m_firstLeft, m_firstRight, m_firstSegment, false);
-            cap(m_lastLeft, m_lastRight, m_lastSegment, true);
+            flushStartCap();
+            flushEndCap();
         }
     }
 
@@ -350,6 +373,8 @@ void Stroker<Rasterizer, VaryingGenerator>::reset()
     m_firstSegment = Segment();
     triangleCount = 0;
     length = 0;
+    m_startCapRendered = false;
+    m_endCapRendered = false;
 }
 
 template <typename Rasterizer, typename VaryingGenerator>

include/quill_stroker.h
@@ -69,6 +69,9 @@ struct Stroker
 
     void reset();
 
+    void flushStartCap();
+    void flushEndCap();
+
     // ********************
     // Internals
     //
@@ -105,9 +108,11 @@ struct Stroker
                 Varyings right = Varyings());
     };
 
+    void cap(Line left, Line right, Segment s, bool endCap);
+
     void store(float x, float y, SegmentType type, Varyings left, Varyings right);
 
-    void cap(Line left, Line right, Segment s, bool endCap);
+
     void join(Line lastLeft, Line lastRight, Line left, Line right, Varyings leftVarying, Varyings rightVarying);
     void stroke(Line left, Line right,
                 Varyings lastLeftVarying, Varyings lastRightVarying,
@@ -121,4 +126,7 @@ struct Stroker
     Segment m_firstSegment;
     Line m_firstLeft;
     Line m_firstRight;
+
+    bool m_startCapRendered = false;
+    bool m_endCapRendered = false;
 };

commit 4205ea7df9d6d248405295bce1d050e3f975c7e3

fix startcap problem introduced with eccc66e9
Some strokes use a dedicated starting length which means that relying on
Stroker::length to decide if we can place the cap or not is not a valid
criteria.

Rather, we need to check if the previous segment was a valid line
segment or not. As we're rejecting identical lineTo's, having a
line means we've moved from the start position.

include/quill_stroker_impl.h
@@ -332,7 +332,7 @@ void Stroker<Rasterizer, VaryingGenerator>::cap(Line left, Line right, Segment s
 template <typename Rasterizer, typename VaryingGenerator>
 void Stroker<Rasterizer, VaryingGenerator>::flushStartCap()
 {
-    if (!m_startCapRendered && length > 0.0f) {
+    if (!m_startCapRendered && m_lastSegment.type == LineToSegment) {
         cap(m_firstLeft, m_firstRight, m_firstSegment, false);
         m_startCapRendered = true;
     }
@@ -343,7 +343,7 @@ void Stroker<Rasterizer, VaryingGenerator>::flushStartCap()
 template <typename Rasterizer, typename VaryingGenerator>
 void Stroker<Rasterizer, VaryingGenerator>::flushEndCap()
 {
-    if (!m_endCapRendered && length > 0.0f) {
+    if (!m_endCapRendered && m_lastSegment.type == LineToSegment) {
         cap(m_lastLeft, m_lastRight, m_lastSegment, true);
         m_endCapRendered = true;
     }